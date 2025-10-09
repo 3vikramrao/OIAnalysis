c5c114c18d920daee2aebb8e468ec133b4bf1069
3vikramrao <3vikramrao@gmail.com>
2025-10-10 01:41:57 +0530

    Update oi_dashboard.py

--- oi_dashboard.py
+++ oi_dashboard.py
@@ -1,116 +1,380 @@
-import streamlit as st
-import pandas as pd
+from datetime import datetime, time
+from time import sleep
 import requests
-import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
+import xlwings as xw
 
-# Function to fetch NIFTY option chain data
-def fetch_option_chain():
-    url = "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
-    headers = {
-        "User-Agent": "Mozilla/5.0",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Referer": "https://www.nseindia.com/"
-    }
-    session = requests.Session()
-    session.get("https://www.nseindia.com", headers=headers)
-    response = session.get(url, headers=headers)
-    data = response.json()
-    return data["records"]["data"]
+#Function to add Strike Price sheet in OptionChain.xlsx
+def addStrikePriceSheet(wb,strikePrice):
+    wb = xw.Book('OptionChain.xlsx')
+    wb.sheets.add(name=str(strikePrice))
 
-# Function to process option chain data
-def process_option_chain(data):
-    rows = []
-    for item in data:
-        strike = item.get("strikePrice")
-        ce_oi = item.get("CE", {}).get("openInterest", 0)
-        pe_oi = item.get("PE", {}).get("openInterest", 0)
-        total_oi = ce_oi + pe_oi
-        rows.append({
-            "Strike Price": strike,
-            "CE OI": ce_oi,
-            "PE OI": pe_oi,
-            "Total OI": total_oi
-        })
-    df = pd.DataFrame(rows)
-    df_sorted = df.sort_values(by="Total OI", ascending=False).reset_index(drop=True)
-    return df_sorted
+#Function add different option strike data in respective option strike sheet
+def addOptionDataInSheet(wb,currTime,strikePrice,ceLTP,ceOI,peLTP,peOI):
+    wb = xw.Book('OptionChain.xlsm')
+    wb_sheet = wb.sheets['Master']
+    if wb_sheet.cells(2, 1).value == None:
+        lastRow = 1
+    else:
+        lastRow = wb_sheet.range('A1').end('down').row
+        print(lastRow)
 
-# Initialize session state for logs
-if "five_min_log" not in st.session_state:
-    st.session_state.five_min_log = []
+    wb_sheet.cells(lastRow + 1, 1).value = currTime
+    wb_sheet.cells(lastRow + 1, 2).value = ceLTP
+    wb_sheet.cells(lastRow + 1, 3).value = ceOI
+    wb_sheet.cells(lastRow + 1, 4).value = strikePrice
+    wb_sheet.cells(lastRow + 1, 5).value = peLTP
+    wb_sheet.cells(lastRow + 1, 6).value = peOI
+    #Color strike Price
+    wb_sheet.range(lastRow+1, 4).color = (255, 255, 0)
 
-if "fifteen_min_log" not in st.session_state:
-    st.session_state.fifteen_min_log = []
+#Function make excel sheet if not present already
+def makeOptionChainFile(opt,currTime):
+    wb = xw.Book('OptionChain.xlsm')
+    optionData = opt["filtered"]["data"]
 
-if "last_fifteen_log_time" not in st.session_state:
-    st.session_state.last_fifteen_log_time = datetime.now() - timedelta(minutes=15)
+    #Create Strike Price worksheet
+    for optionRow in optionData:
+        strikePrice = optionRow["CE"]["strikePrice"]
+        ceLTP = optionRow["CE"]["lastPrice"]
+        ceOI = 75*optionRow["CE"]["openInterest"]
+        peLTP = optionRow["PE"]["lastPrice"]
+        peOI = 75*optionRow["PE"]["openInterest"]
+        addOptionDataInSheet(wb,currTime,strikePrice,ceLTP,ceOI,peLTP,peOI)
+    wb.save()
 
-st.title("NIFTY OI Analysis Dashboard")
+#Function to add data in optionchain.xlsm
+def putOptionChainData(opt,currTime):
+    wb = xw.Book('OptionChain.xlsm')
+    optionData = opt["filtered"]["data"]
+    for optionRow in optionData:
+        strikePrice = optionRow["CE"]["strikePrice"]
+        ceLTP = optionRow["CE"]["lastPrice"]
+        ceOI = 75*optionRow["CE"]["openInterest"]
+        peLTP = optionRow["PE"]["lastPrice"]
+        peOI = 75*optionRow["PE"]["openInterest"]
+        addOptionDataInSheet(wb,currTime,strikePrice,ceLTP,ceOI,peLTP,peOI)
+    wb.save()
 
-# Sidebar button to fetch data
-if st.sidebar.button("Fetch Latest Data"):
+#Function to add 5 min data inside OiAnalysis.xlsx
+def putInExcel5Min(currTraded, futOI, tradedVolCon, callOI, putOI,currTime):
+
+    wb =xw.Book('OiAnalysis.xlsx')
+    wb_sheet = wb.sheets[0]
+    lastRow = wb_sheet.range('A1').end('down').row
+    last_time = wb_sheet.cells(lastRow, 1).value
+    last_timee = last_time.split('-')
+    time_range = last_timee[1] + "-" + currTime
+    last_traded_price = wb_sheet.cells(lastRow, 2).value
+    changeInLTP = currTraded - last_traded_price
+    last_Fut_OI = wb_sheet.cells(lastRow,5).value
+    changeInFutOI = futOI - last_Fut_OI
+    last_Call_OI = wb_sheet.cells(lastRow, 7).value
+    changeInCallOI = callOI - last_Call_OI
+    last_Put_OI = wb_sheet.cells(lastRow,9).value
+    changeInPutOI = putOI - last_Put_OI
+    OiInter = ""
+    signal = ""
+    redFill = (255,204,203)
+    greenFill = (144,238,144)
+
+    if changeInLTP > 0 and changeInFutOI > 0:
+        OiInter = "Long Buildup"
+    elif changeInLTP < 0 and changeInFutOI > 0:
+        OiInter = "Short Buildup"
+    elif changeInLTP < 0 and changeInFutOI < 0:
+        OiInter = "Long Unwinding"
+    elif changeInLTP > 0 and changeInFutOI < 0:
+        OiInter = "Short Covering"
+
+    if changeInLTP>0 and changeInFutOI>0 and changeInPutOI>0 and changeInCallOI<0:
+        signal = "Buy"
+    elif changeInLTP<0 and changeInFutOI>0 and changeInPutOI<0 and changeInCallOI>0:
+        signal = "Sell"
+    elif changeInLTP<0 and changeInFutOI<0 and changeInPutOI<0 and changeInCallOI>0:
+        signal = "Sell"
+    elif changeInLTP>0 and changeInFutOI<0 and changeInPutOI>0 and changeInCallOI<0:
+        signal = "Buy"
+
+    #color filling start
+    if changeInLTP>0:
+        wb_sheet.range(lastRow + 1, 3).color = greenFill
+    elif changeInLTP<0:
+        wb_sheet.range(lastRow + 1, 3).color = redFill
+
+    if changeInFutOI>0:
+        wb_sheet.range(lastRow + 1, 6).color = greenFill
+    elif changeInFutOI<0:
+        wb_sheet.range(lastRow + 1, 6).color = redFill
+
+    if changeInCallOI>0:
+        wb_sheet.range(lastRow + 1, 8).color = greenFill
+    elif changeInCallOI<0:
+        wb_sheet.range(lastRow + 1, 8).color = redFill
+
+    if changeInPutOI>0:
+        wb_sheet.range(lastRow + 1, 10).color = greenFill
+    elif changeInPutOI<0:
+        wb_sheet.range(lastRow + 1, 10).color = redFill
+
+    if signal=="Buy":
+        wb_sheet.range(lastRow + 1, 12).color = greenFill
+    elif signal=="Sell":
+        wb_sheet.range(lastRow + 1, 12).color = redFill
+
+    wb_sheet.cells(lastRow+1, 1).value = time_range
+    wb_sheet.cells(lastRow+1, 2).value = currTraded
+    wb_sheet.cells(lastRow+1, 3).value = changeInLTP
+    wb_sheet.cells(lastRow+1, 4).value = tradedVolCon
+    wb_sheet.cells(lastRow+1, 5).value = futOI
+    wb_sheet.cells(lastRow+1, 6).value = changeInFutOI
+    wb_sheet.cells(lastRow+1, 7).value = callOI
+    wb_sheet.cells(lastRow+1, 8).value = changeInCallOI
+    wb_sheet.cells(lastRow+1, 9).value = putOI
+    wb_sheet.cells(lastRow+1, 10).value = changeInPutOI
+    wb_sheet.cells(lastRow + 1, 11).value = OiInter
+    wb_sheet.cells(lastRow + 1, 12).value = signal
+    wb.save()
+    print(time_range,"    ",currTraded,"    ",changeInLTP,"     ",OiInter,"      ",signal)
+
+
+#Function to add 15 min data inside OiAnalysis.xlsx
+def putInExcel15Min(currTraded, futOI, tradedVolCon, callOI, putOI,currTime):
+    wb = xw.Book('OiAnalysis.xlsx')
+    wb_sheet = wb.sheets[1]
+    lastRow = wb_sheet.range('A1').end('down').row
+    last_time = wb_sheet.cells(lastRow, 1).value
+    last_timee = last_time.split('-')
+    time_range = last_timee[1] + "-" + currTime
+    last_traded_price = wb_sheet.cells(lastRow, 2).value
+    changeInLTP = currTraded - last_traded_price
+    last_Fut_OI = wb_sheet.cells(lastRow, 5).value
+    changeInFutOI = futOI - last_Fut_OI
+    last_Call_OI = wb_sheet.cells(lastRow, 7).value
+    changeInCallOI = callOI - last_Call_OI
+    last_Put_OI = wb_sheet.cells(lastRow, 9).value
+    changeInPutOI = putOI - last_Put_OI
+    OiInter = ""
+    signal = ""
+    redFill = (255, 204, 203)
+    greenFill = (144, 238, 144)
+
+    if changeInLTP > 0 and changeInFutOI > 0:
+        OiInter = "Long Buildup"
+    elif changeInLTP < 0 and changeInFutOI > 0:
+        OiInter = "Short Buildup"
+    elif changeInLTP < 0 and changeInFutOI < 0:
+        OiInter = "Long Unwinding"
+    elif changeInLTP > 0 and changeInFutOI < 0:
+        OiInter = "Short Covering"
+
+    if changeInLTP > 0 and changeInFutOI > 0 and changeInPutOI > 0 and changeInCallOI < 0:
+        signal = "Buy"
+    elif changeInLTP < 0 and changeInFutOI > 0 and changeInPutOI < 0 and changeInCallOI > 0:
+        signal = "Sell"
+    elif changeInLTP < 0 and changeInFutOI < 0 and changeInPutOI < 0 and changeInCallOI > 0:
+        signal = "Sell"
+    elif changeInLTP > 0 and changeInFutOI < 0 and changeInPutOI > 0 and changeInCallOI < 0:
+        signal = "Buy"
+
+    # color filling start
+    if changeInLTP > 0:
+        wb_sheet.range(lastRow + 1, 3).color = greenFill
+    elif changeInLTP < 0:
+        wb_sheet.range(lastRow + 1, 3).color = redFill
+
+    if changeInFutOI > 0:
+        wb_sheet.range(lastRow + 1, 6).color = greenFill
+    elif changeInFutOI < 0:
+        wb_sheet.range(lastRow + 1, 6).color = redFill
+
+    if changeInCallOI > 0:
+        wb_sheet.range(lastRow + 1, 8).color = greenFill
+    elif changeInCallOI < 0:
+        wb_sheet.range(lastRow + 1, 8).color = redFill
+
+    if changeInPutOI > 0:
+        wb_sheet.range(lastRow + 1, 10).color = greenFill
+    elif changeInPutOI < 0:
+        wb_sheet.range(lastRow + 1, 10).color = redFill
+
+    if signal == "Buy":
+        wb_sheet.range(lastRow + 1, 12).color = greenFill
+    elif signal == "Sell":
+        wb_sheet.range(lastRow + 1, 12).color = redFill
+
+    wb_sheet.cells(lastRow + 1, 1).value = time_range
+    wb_sheet.cells(lastRow + 1, 2).value = currTraded
+    wb_sheet.cells(lastRow + 1, 3).value = changeInLTP
+    wb_sheet.cells(lastRow + 1, 4).value = tradedVolCon
+    wb_sheet.cells(lastRow + 1, 5).value = futOI
+    wb_sheet.cells(lastRow + 1, 6).value = changeInFutOI
+    wb_sheet.cells(lastRow + 1, 7).value = callOI
+    wb_sheet.cells(lastRow + 1, 8).value = changeInCallOI
+    wb_sheet.cells(lastRow + 1, 9).value = putOI
+    wb_sheet.cells(lastRow + 1, 10).value = changeInPutOI
+    wb_sheet.cells(lastRow + 1, 11).value = OiInter
+    wb_sheet.cells(lastRow + 1, 12).value = signal
+    wb.save()
+
+def enterInExcel(currTime,isFifteenMin):
     try:
-        option_chain_data = fetch_option_chain()
-        now = datetime.now().strftime("%H:%M:%S")
+        url1 = "https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/ajaxFOGetQuoteJSON.jsp?underlying=NIFTY&instrument=FUTIDX&expiry=24SEP2020&type=SELECT&strike=11500.00"
+        referer = "https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuoteFO.jsp?underlying=NIFTY&instrument=OPTIDX&expiry=17SEP2020&type=CE&strike=11500.00"
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
+            'Accept-Language': 'en-US,en;q=0.9', 'Accept-Encoding': 'gzip, deflate, br', 'referer': referer}
+        cookies = {
+            'bm_sv': '808AA9DAA6889DCF6CB5A9AB14BF290F~t420SFeAK8epqchwzNEdgzJGSy4pjs8Mco4O/d7w5qW4TxShiDJYDHoeSAe60uaZMH2n5wjH3v0NnmtPWrKZosEZDfOSHts34ur4GGAPsGGL3LHYVPU/IbcIDhNf6MXF8oqWfpyJ0FXh74VTcL2NhnLG6DlsLun9OrT/+t9vHpY='}
+        session = requests.session()
+        for cook in cookies:
+            session.cookies.set(cook, cookies[cook])
 
-        # Aggregate CE and PE OI changes
-        ce_oi_total = sum(item.get("CE", {}).get("openInterest", 0) for item in option_chain_data)
-        pe_oi_total = sum(item.get("PE", {}).get("openInterest", 0) for item in option_chain_data)
+        fut = session.get(url1, headers=headers).json()
+        url2 = "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
+        session = requests.session()
 
-        # Calculate PCR
-        pcr = round(pe_oi_total / ce_oi_total, 2) if ce_oi_total else 0
+        for cook in cookies:
+            session.cookies.set(cook, cookies[cook])
+        opt = session.get(url2, headers=headers).json()
+        # Future OI
+        lastTraded = float((fut["data"][0]["lastPrice"]).replace(',', ''))
+        futOI = int((fut["data"][0]["openInterest"]).replace(',', ''))
+        tradedVolCon = int((fut["data"][0]["numberOfContractsTraded"]).replace(',', ''))
 
-        # Generate signal
-        signal = "Buy" if pcr > 1 else "Sell"
-
-        # Log 5-min data
-        st.session_state.five_min_log.append({
-            "Time": now,
-            "CE OI": ce_oi_total,
-            "PE OI": pe_oi_total,
-            "PCR": pcr,
-            "Signal": signal
-        })
-
-        # Log 15-min data if time elapsed
-        current_time = datetime.now()
-        if current_time - st.session_state.last_fifteen_log_time >= timedelta(minutes=15):
-            st.session_state.fifteen_min_log.append({
-                "Time": now,
-                "CE OI": ce_oi_total,
-                "PE OI": pe_oi_total,
-                "PCR": pcr,
-                "Signal": signal
-            })
-            st.session_state.last_fifteen_log_time = current_time
+        # Option OI
+        callOI = opt["filtered"]["CE"]["totOI"]
+        putOI = opt["filtered"]["PE"]["totOI"]
+        putInExcel5Min(lastTraded, futOI, tradedVolCon, callOI, putOI,currTime)
+        putOptionChainData(opt,currTime)
+        if isFifteenMin:
+            putInExcel15Min(lastTraded, futOI, tradedVolCon, callOI, putOI,currTime)
 
     except Exception as e:
-        st.error(f"Error fetching data: {e}")
+        print(e)
 
-# Display 5-min log
-st.subheader("5-Minute Log")
-df_5min = pd.DataFrame(st.session_state.five_min_log)
-st.dataframe(df_5min)
+def putInExcelIni(lastTraded, futOI, tradedVolCon, callOI, putOI,currTime):
+    wb = xw.Book('OiAnalysis.xlsx')
+    sht5min = wb.sheets[0]
+    sht15min = wb.sheets[1]
+    sht5min.cells(1, 1).value = "Time"
+    sht5min.cells(1, 2).value = "LTP"
+    sht5min.cells(1, 3).value = "Change in LTP"
+    sht5min.cells(1, 4).value = "Traded Volume(Contract)"
+    sht5min.cells(1, 5).value = "Future OI"
+    sht5min.cells(1, 6).value = "Change Future OI"
+    sht5min.cells(1, 7).value = "Call OI"
+    sht5min.cells(1, 8).value = "Change Call OI"
+    sht5min.cells(1, 9).value = "Put OI"
+    sht5min.cells(1, 10).value = "Change Put OI"
+    sht5min.cells(1, 11).value = "OI Interpretation"
+    sht5min.cells(1, 12).value = "Buy/Sell"
 
-# Display PCR trend chart
-if not df_5min.empty:
-    st.subheader("PCR Trend Chart")
-    fig, ax = plt.subplots()
-    ax.plot(df_5min["Time"], df_5min["PCR"], marker='o')
-    ax.set_xlabel("Time")
-    ax.set_ylabel("PCR")
-    ax.set_title("PCR Trend Over Time")
-    plt.xticks(rotation=45)
-    st.pyplot(fig)
+    sht5min.range('A1:L1').color = (255,255,0)
 
-# Display 15-min log
-st.subheader("15-Minute Log")
-df_15min = pd.DataFrame(st.session_state.fifteen_min_log)
-st.dataframe(df_15min)
+    sht5min.cells(2, 1).value = "00:00-"+currTime
+    sht5min.cells(2, 2).value = lastTraded
+    sht5min.cells(2, 3).value = 0
+    sht5min.cells(2, 4).value = tradedVolCon
+    sht5min.cells(2, 5).value = futOI
+    sht5min.cells(2, 6).value = 0
+    sht5min.cells(2, 7).value = callOI
+    sht5min.cells(2, 8).value = 0
+    sht5min.cells(2, 9).value = putOI
+    sht5min.cells(2, 10).value = 0
 
-# Display Option Chain OI Analysis
-st.subheader("Nifty Option Chain OI Analysis")
-if "option_chain_data" in locals():
-    df_option_chain = process_option_chain(option_chain_data)
-    st.dataframe(df_option_chain)
+    sht15min.cells(1, 1).value = "Time"
+    sht15min.cells(1, 2).value = "LTP"
+    sht15min.cells(1, 3).value = "Change in LTP"
+    sht15min.cells(1, 4).value = "Traded Volume(Contract)"
+    sht15min.cells(1, 5).value = "Future OI"
+    sht15min.cells(1, 6).value = "Change Future OI"
+    sht15min.cells(1, 7).value = "Call OI"
+    sht15min.cells(1, 8).value = "Change Call OI"
+    sht15min.cells(1, 9).value = "Put OI"
+    sht15min.cells(1, 10).value = "Change Put OI"
+    sht15min.cells(1, 11).value = "OI Interpretation"
+    sht15min.cells(1, 12).value = "Buy/Sell"
+
+    sht15min.range('A1:L1').color = (255, 255, 0)
+
+    sht15min.cells(2, 1).value = "00:00-"+currTime
+    sht15min.cells(2, 2).value = lastTraded
+    sht15min.cells(2, 3).value = 0
+    sht15min.cells(2, 4).value = tradedVolCon
+    sht15min.cells(2, 5).value = futOI
+    sht15min.cells(2, 6).value = 0
+    sht15min.cells(2, 7).value = callOI
+    sht15min.cells(2, 8).value = 0
+    sht15min.cells(2, 9).value = putOI
+    sht15min.cells(2, 10).value = 0
+    wb.save()
+
+def initializeFiles(currTime):
+    try:
+        url1 = "https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/ajaxFOGetQuoteJSON.jsp?underlying=NIFTY&instrument=FUTIDX&expiry=24SEP2020&type=SELECT&strike=11500.00"
+        referer = "https://www1.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuoteFO.jsp?underlying=NIFTY&instrument=OPTIDX&expiry=17SEP2020&type=CE&strike=11500.00"
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
+            'Accept-Language': 'en-US,en;q=0.9', 'Accept-Encoding': 'gzip, deflate, br', 'referer': referer}
+        cookies = {
+            'bm_sv': '808AA9DAA6889DCF6CB5A9AB14BF290F~t420SFeAK8epqchwzNEdgzJGSy4pjs8Mco4O/d7w5qW4TxShiDJYDHoeSAe60uaZMH2n5wjH3v0NnmtPWrKZosEZDfOSHts34ur4GGAPsGGL3LHYVPU/IbcIDhNf6MXF8oqWfpyJ0FXh74VTcL2NhnLG6DlsLun9OrT/+t9vHpY='}
+        session = requests.session()
+        for cook in cookies:
+            session.cookies.set(cook, cookies[cook])
+
+        fut = session.get(url1, headers=headers).json()
+        url2 = "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
+        session = requests.session()
+
+        for cook in cookies:
+            session.cookies.set(cook, cookies[cook])
+        opt = session.get(url2, headers=headers).json()
+        # Future OI
+        lastTraded = float((fut["data"][0]["lastPrice"]).replace(',', ''))
+        futOI = int((fut["data"][0]["openInterest"]).replace(',', ''))
+        tradedVolCon = int((fut["data"][0]["numberOfContractsTraded"]).replace(',', ''))
+
+        # Option OI
+        callOI = opt["filtered"]["CE"]["totOI"]
+        putOI = opt["filtered"]["PE"]["totOI"]
+
+        putInExcelIni(lastTraded, futOI, tradedVolCon, callOI, putOI,currTime)
+        makeOptionChainFile(opt,currTime)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+if __name__ == '__main__':
+   success = False
+
+   wb = xw.Book()
+   wb.save('OiAnalysis.xlsx')
+   wb.sheets.add(name='FiftMin')
+   wb.sheets.add(name='FiveMin')
+   for sheet in wb.sheets:
+       if 'Sheet' in sheet.name:
+           sheet.delete()
+   # This loop runs from 9:15 AM to 3:30 PM till Market hours
+   while(1):
+        t1 = datetime.now()
+        currTime = str(t1.hour)+":"+str(t1.minute)
+        if(time(9,15)<=datetime.now().time()<=time(9,17,30) and not(success)):
+            success = initializeFiles(currTime)
+
+        isFifMin=1
+        while(time(9,20)<=datetime.now().time()<=time(15,31)):
+            t1 = datetime.now()
+            currTime = str(t1.hour) + ":" + str(t1.minute)
+            isFifMin+=1
+            isFifteenMin = False
+            if isFifMin==3:
+                isFifteenMin = True
+                isFifMin=0
+            enterInExcel(currTime,isFifteenMin)
+            t1 = datetime.now()
+            while(t1.minute%5 !=0 or t1.second !=0):
+                t1 = datetime.now()
+                sleep(1)
+        sleep(1)
